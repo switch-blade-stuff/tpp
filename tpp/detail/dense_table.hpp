c182d7d989437b9341d4c6e20064dde3bfcd37f6
switchblade <terekhin.mikhail.01@gmail.com>
2022-11-15 19:14:14 -0800

    Implemented dense table insertion, removal & lookup operations

--- tpp/detail/dense_table.hpp
+++ tpp/detail/dense_table.hpp
@@ -10,6 +10,7 @@
 
 #include <vector>
 #include <limits>
+#include <tuple>
 
 #endif
 
@@ -25,6 +26,20 @@ namespace tpp::detail
 
 		template<typename... Args>
 		constexpr dense_bucket_node(Args &&...args) noexcept(nothrow_ctor<V, Args...>) : value_base(std::forward<Args>(args)...) {}
+
+		template<typename T>
+		constexpr void replace(T &&new_value, std::size_t new_hash)
+		{
+			/* If move-assign is possible, do that. Otherwise, re-init the value. */
+			if constexpr (std::is_assignable_v<V, T &&>)
+				value() = std::forward<T>(new_value);
+			else
+			{
+				std::destroy_at(get());
+				std::construct_at(get(), std::forward<T>(new_value));
+			}
+			hash = new_hash;
+		}
 
 		[[nodiscard]] constexpr V *get() noexcept { return &value(); }
 		[[nodiscard]] constexpr const V *get() const noexcept { return &value(); }
@@ -100,8 +115,8 @@ namespace tpp::detail
 		typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
 	private:
-		[[nodiscard]] constexpr static iterator to_iter(bucket_node *node) noexcept { return iterator{node_iterator{node}}; }
-		[[nodiscard]] constexpr static const_iterator to_iter(const bucket_node *node) noexcept { return const_iterator{const_node_iterator{node}}; }
+		[[nodiscard]] constexpr static auto to_iter(bucket_node *node) noexcept { return iterator{node_iterator{node}}; }
+		[[nodiscard]] constexpr static auto to_iter(const bucket_node *node) noexcept { return const_iterator{const_node_iterator{node}}; }
 
 	public:
 		constexpr dense_table() = default;
@@ -185,58 +200,111 @@ namespace tpp::detail
 			return *this;
 		}
 
-		[[nodiscard]] constexpr iterator begin() noexcept
-		{
-			if constexpr (is_ordered::value)
-				return iterator{node_iterator{header().next}};
-			else
-				return iterator{m_dense.data()};
-		}
-		[[nodiscard]] constexpr const_iterator begin() const noexcept
-		{
-			if constexpr (is_ordered::value)
-				return const_iterator{node_iterator{header().next}};
-			else
-				return const_iterator{m_dense.data()};
-		}
-		[[nodiscard]] constexpr iterator end() noexcept
-		{
-			if constexpr (is_ordered::value)
-				return iterator{node_iterator{&header()}};
-			else
-				return iterator{m_dense.data() + size()};
-		}
-		[[nodiscard]] constexpr const_iterator end() const noexcept
-		{
-			if constexpr (is_ordered::value)
-				return const_iterator{node_iterator{&header()}};
-			else
-				return const_iterator{m_dense.data() + size()};
-		}
+		[[nodiscard]] constexpr iterator begin() noexcept { return to_iter(begin_node()); }
+		[[nodiscard]] constexpr const_iterator begin() const noexcept { return to_iter(begin_node()); }
+		[[nodiscard]] constexpr iterator end() noexcept { return to_iter(end_node()); }
+		[[nodiscard]] constexpr const_iterator end() const noexcept { return to_iter(end_node()); }
 
 		[[nodiscard]] constexpr reverse_iterator rbegin() noexcept { return reverse_iterator{end()}; }
 		[[nodiscard]] constexpr const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator{end()}; }
 		[[nodiscard]] constexpr reverse_iterator rend() noexcept { return reverse_iterator{begin()}; }
 		[[nodiscard]] constexpr const_reverse_iterator rend() const noexcept { return const_reverse_iterator{begin()}; }
 
+		[[nodiscard]] constexpr value_type &front() noexcept { return front_node()->value(); }
+		[[nodiscard]] constexpr const value_type &front() const noexcept { return front_node()->value(); }
+		[[nodiscard]] constexpr value_type &back() noexcept { return back_node()->value(); }
+		[[nodiscard]] constexpr const value_type &back() const noexcept { return back_node()->value(); }
+
 		[[nodiscard]] constexpr size_type size() const noexcept { return m_dense.size(); }
 		[[nodiscard]] constexpr size_type capacity() const noexcept { return to_capacity(bucket_count()); }
 		[[nodiscard]] constexpr size_type max_size() const noexcept { return to_capacity(std::min(m_dense.max_size(), npos - 1)); }
-		[[nodiscard]] constexpr float load_factor() const noexcept
-		{
-			return static_cast<float>(size()) / static_cast<float>(bucket_count());
-		}
+		[[nodiscard]] constexpr float load_factor() const noexcept { return static_cast<float>(size()) / static_cast<float>(bucket_count()); }
 
 		[[nodiscard]] constexpr size_type bucket_count() const noexcept { return m_sparse.size(); }
 		[[nodiscard]] constexpr size_type max_bucket_count() const noexcept { return m_sparse.max_size(); }
 
-		[[nodiscard]] constexpr iterator find(const auto &key) noexcept { return to_iter(m_dense.data() + find_impl(key_hash(key), key)); }
-		[[nodiscard]] constexpr const_iterator find(const auto &key) const noexcept { return to_iter(m_dense.data() + find_impl(key_hash(key), key)); }
+		[[nodiscard]] constexpr bool contains(const auto &key) const noexcept { return *find_node(key_hash(key), key).second != npos; }
+
+		[[nodiscard]] constexpr iterator find(const auto &key) noexcept { return to_iter(find_node(key_hash(key), key).first); }
+		[[nodiscard]] constexpr const_iterator find(const auto &key) const noexcept { return to_iter(find_node(key_hash(key), key).first); }
+
+		constexpr std::pair<iterator, bool> insert(const value_type &value) { return insert_impl({}, KGet{}(value), value); }
+		constexpr std::pair<iterator, bool> insert(value_type &&value) { return insert_impl({}, KGet{}(value), std::move(value)); }
+		constexpr iterator insert(const_iterator hint, const value_type &value) { return insert_impl(to_underlying(hint), KGet{}(value), value).first; }
+		constexpr iterator insert(const_iterator hint, value_type &&value) { return insert_impl(to_underlying(hint), KGet{}(value), std::move(value)).first; }
+
+		template<typename T, typename = std::enable_if_t<!std::is_convertible_v<T &&, value_type &&>>>
+		constexpr std::pair<iterator, bool> insert(T &&value) TPP_REQUIRES((std::is_constructible_v<V, T>))
+		{
+			return emplace_impl({}, std::forward<T>(value));
+		}
+		template<typename T, typename = std::enable_if_t<!std::is_convertible_v<T &&, value_type &&>>>
+		constexpr iterator insert(const_iterator hint, T &&value) TPP_REQUIRES((std::is_constructible_v<V, T>))
+		{
+			return emplace_impl(to_underlying(hint), std::forward<T>(value)).first;
+		}
+
+		template<typename T, typename = std::enable_if_t<!std::is_same_v<std::decay_t<K>, std::decay_t<V>>>>
+		constexpr std::pair<iterator, bool> insert_or_assign(const auto &key, T &&value) TPP_REQUIRES((std::is_constructible_v<V, T>))
+		{
+			return insert_or_assign_impl({}, key, std::forward<T>(value));
+		}
+		template<typename T, typename = std::enable_if_t<!std::is_same_v<std::decay_t<K>, std::decay_t<V>>>>
+		constexpr iterator insert_or_assign(const_iterator hint, const auto &key, T &&value) TPP_REQUIRES((std::is_constructible_v<V, T>))
+		{
+			return insert_or_assign_impl(to_underlying(hint), key, std::forward<T>(value)).first;
+		}
+
+		template<typename... Args>
+		constexpr std::pair<iterator, bool> emplace(Args &&...args) TPP_REQUIRES((std::is_constructible_v<V, Args...>))
+		{
+			return emplace_impl({}, std::forward<Args>(args)...);
+		}
+		template<typename... Args>
+		constexpr iterator emplace_hint(const_iterator hint, Args &&...args) TPP_REQUIRES((std::is_constructible_v<V, Args...>))
+		{
+			return emplace_impl(to_underlying(hint), std::forward<Args>(args)...);
+		}
+
+		template<typename... Args, typename = std::enable_if_t<!std::is_same_v<std::decay_t<K>, std::decay_t<V>>>>
+		constexpr std::pair<iterator, bool> emplace_or_replace(Args &&...args) TPP_REQUIRES((std::is_constructible_v<V, Args...>))
+		{
+			return emplace_or_replace_impl({}, std::forward<Args>(args)...);
+		}
+		template<typename... Args, typename = std::enable_if_t<!std::is_same_v<std::decay_t<K>, std::decay_t<V>>>>
+		constexpr iterator emplace_or_replace(const_iterator hint, Args &&...args) TPP_REQUIRES((std::is_constructible_v<V, Args...>))
+		{
+			return emplace_or_replace_impl(to_underlying(hint), std::forward<Args>(args)...).first;
+		}
+
+		template<typename U, typename... Args, typename = std::enable_if_t<!std::is_same_v<std::decay_t<K>, std::decay_t<V>>>>
+		constexpr std::pair<iterator, bool> try_emplace(U &&key, Args &&...args) TPP_REQUIRES(
+				(std::is_constructible_v<V, std::piecewise_construct_t, std::tuple<U &&>, std::tuple<Args && ...>>))
+		{
+			return emplace_impl({}, hash(key), key, std::piecewise_construct,
+			                    std::forward_as_tuple(std::forward<U>(key)),
+			                    std::forward_as_tuple(std::forward<Args>(args)...));
+		}
+		template<typename U, typename... Args, typename = std::enable_if_t<!std::is_same_v<std::decay_t<K>, std::decay_t<V>>>>
+		constexpr iterator try_emplace(const_iterator hint, U &&key, Args &&...args) TPP_REQUIRES(
+				(std::is_constructible_v<V, std::piecewise_construct_t, std::tuple<U &&>, std::tuple<Args && ...>>))
+		{
+			return emplace_impl(to_underlying(hint), hash(key), key, std::piecewise_construct,
+			                    std::forward_as_tuple(std::forward<U>(key)),
+			                    std::forward_as_tuple(std::forward<Args>(args)...)).first;
+		}
 
 		constexpr void clear()
 		{
 			std::fill_n(m_sparse.data(), bucket_count(), npos);
 			m_dense.clear();
+
+			/* Update header link. */
+			if constexpr (is_ordered::value)
+			{
+				auto *h = header_link();
+				h->relink(*h, *h);
+			}
 		}
 
 		constexpr void rehash(size_type new_cap)
@@ -249,8 +317,22 @@ namespace tpp::detail
 		}
 		constexpr void reserve(size_type n)
 		{
+			[[maybe_unused]] const auto front_pos = front_node() - m_dense.data();
+			[[maybe_unused]] const auto back_pos = back_node() - m_dense.data();
+
 			m_dense.reserve(n);
 			rehash(static_cast<size_type>(static_cast<float>(n) / max_load_factor));
+
+			if constexpr (is_ordered::value)
+			{
+				/* Update header link only if it does not point to itself. */
+				if (!m_dense.empty())
+				{
+					auto *new_front = m_dense.data() + front_pos;
+					auto *new_back = m_dense.data() + back_pos;
+					header_link()->relink(*new_front, *new_back);
+				}
+			}
 		}
 
 		[[nodiscard]] constexpr auto allocator() const noexcept { return m_dense.get_allocator(); }
@@ -270,107 +352,130 @@ namespace tpp::detail
 		}
 
 	private:
-		[[nodiscard]] constexpr bucket_link &header() noexcept { return header_base::value(); }
-		[[nodiscard]] constexpr const bucket_link &header() const noexcept { return header_base::value(); }
+		[[nodiscard]] constexpr auto to_capacity(size_type n) const noexcept { return static_cast<size_type>(static_cast<float>(n) * max_load_factor); }
 
-		constexpr void relink_header() noexcept
+		/* Using `const_cast` here to avoid non-const function duplicates. Pointers will be converted to appropriate const-ness either way. */
+		[[nodiscard]] constexpr auto *header_link() const noexcept { return const_cast<bucket_link *>(&header_base::value()); }
+		[[nodiscard]] constexpr auto *begin_node() const noexcept
 		{
 			if constexpr (is_ordered::value)
-			{
-				if (auto &h = header(); !m_dense.empty())
-					h.relink(h.next, h.prev);
-			}
+				return static_cast<bucket_node *>(header_link()->next);
+			else
+				return const_cast<bucket_node *>(m_dense.data());
 		}
+		[[nodiscard]] constexpr auto *front_node() const noexcept { return begin_node(); }
+		[[nodiscard]] constexpr auto *back_node() const noexcept
+		{
+			if constexpr (is_ordered::value)
+				return static_cast<bucket_node *>(header_link().prev);
+			else
+				return const_cast<bucket_node *>(m_dense.data()) + (m_dense.size() - 1);
+		}
+		[[nodiscard]] constexpr auto *end_node() const noexcept
+		{
+			if constexpr (is_ordered::value)
+				return static_cast<bucket_node *>(header_link());
+			else
+				return const_cast<bucket_node *>(m_dense.data()) + m_dense.size();
+		}
+
+		/* Same reason for `const_cast` as with node getters above. */
+		[[nodiscard]] constexpr auto *get_chain(std::size_t h) const noexcept { return const_cast<size_type *>(m_sparse.data()) + (h % bucket_count()); }
 
 		[[nodiscard]] constexpr auto hash(const auto &k) const { return get_hash()(k); }
 		[[nodiscard]] constexpr auto cmp(const auto &a, const auto &b) const { return get_cmp()(a, b); }
 
-		[[nodiscard]] constexpr size_type to_capacity(size_type n) const noexcept
+		/* Return both target bucket & it's chain pointer to use with replace operations. */
+		[[nodiscard]] constexpr auto find_node(std::size_t h, const auto &key) const noexcept -> std::pair<bucket_node *, size_type *>
 		{
-			return static_cast<size_type>(static_cast<float>(n) * max_load_factor);
+			auto *idx = get_chain(h);
+			while (*idx != npos)
+			{
+				auto &entry = m_dense[*idx];
+				if (entry.hash == h && cmp(key, entry.key()))
+					return {&entry, idx};
+				idx = &entry.next;
+			}
+			return {end_node(), idx};
 		}
 
-		[[nodiscard]] constexpr size_type *get_chain(std::size_t h) noexcept { return m_sparse.data() + (h % bucket_count()); }
-		[[nodiscard]] constexpr const size_type *get_chain(std::size_t h) const noexcept { return m_sparse.data() + (h % bucket_count()); }
-
-		[[nodiscard]] constexpr size_type find_impl(std::size_t h, const auto &key) const noexcept
+		constexpr iterator commit_node([[maybe_unused]] const_node_iterator hint, size_type pos, auto *chain_idx, std::size_t h, bucket_node *node)
 		{
-			for (auto *idx = get_chain(h); *idx != npos;)
-				if (auto &entry = m_dense[*idx]; entry.hash == h && cmp(key, entry.key()))
-					return *idx;
-				else
-					idx = &entry.next;
-			return m_dense.size();
+			/* Create the bucket and insertion order links. */
+			if constexpr (is_ordered::value) node->link(hint.link ? *hint.link : *back_node());
+			*chain_idx = pos;
+
+			/* Initialize the hash & rehash the table. Doing it only now
+			 * so that any temporary nodes do not affect table hashing. */
+			node->hash = h;
+			maybe_rehash();
+
+			return to_iter(node);
 		}
 
 		template<typename... Args>
-		[[nodiscard]] constexpr iterator insert_new(std::size_t h, auto *chain_idx, Args &&...args)
+		constexpr auto emplace_impl(const_node_iterator hint, Args &&...args) -> std::pair<iterator, bool>
 		{
-			const auto pos = *chain_idx = size();
-			m_dense.emplace_back(std::forward<Args>(args)...).hash = h;
-			maybe_rehash();
-			return to_iter(m_dense.data() + pos);
+			/* Create a temporary object to check if it already exists within the table. */
+			const auto pos = size();
+			auto &tmp = m_dense.emplace_back(std::forward<Args>(args)...);
+			const auto hash = key_hash(tmp.key());
+
+			/* If there is no conflict, commit the temporary to the table. Otherwise, destroy the temporary. */
+			if (const auto [candidate, chain_idx] = find_node(hash, tmp.key()); *chain_idx == npos)
+				return {commit_node(hint, pos, chain_idx, hash, &tmp), true};
+			else
+			{
+				m_dense.pop_back();
+				return {to_iter(candidate), false};
+			}
+		}
+		template<typename... Args>
+		constexpr auto emplace_or_replace_impl(const_node_iterator hint, Args &&...args) -> std::pair<iterator, bool>
+		{
+			/* Create a temporary object to check if it already exists within the table. */
+			const auto pos = size();
+			auto &tmp = m_dense.emplace_back(std::forward<Args>(args)...);
+			const auto hash = key_hash(tmp.key());
+
+			/* If there is no conflict, commit the temporary to the table. Otherwise, replace the existing with the temporary. */
+			if (const auto [candidate, chain_idx] = find_node(hash, tmp.key()); *chain_idx == npos)
+				return {commit_node(hint, pos, chain_idx, hash, &tmp), true};
+			else
+			{
+				candidate->replace(std::move(tmp.value()));
+				m_dense.pop_back();
+				return {to_iter(candidate), false};
+			}
+		}
+
+		template<typename... Args>
+		constexpr iterator emplace_at(const_node_iterator hint, auto *chain_idx, std::size_t h, Args &&...args)
+		{
+			return commit_node(hint, size(), chain_idx, h, &m_dense.emplace_back(std::forward<Args>(args)...));
+		}
+
+		template<typename T>
+		constexpr auto insert_impl(const_node_iterator hint, const auto &key, T &&value) -> std::pair<iterator, bool>
+		{
+			/* If a candidate was found, do nothing. Otherwise, emplace a new entry. */
+			const auto h = hash(key);
+			if (const auto [candidate, chain_idx] = find_node(h, key); *chain_idx == npos)
+				return {emplace_at(hint, chain_idx, h, std::forward<T>(value)), true};
+			else
+				return {to_iter(candidate), false};
 		}
 		template<typename T>
-		[[nodiscard]] constexpr std::pair<iterator, bool> emplace_impl(const auto &key, T &&value)
+		constexpr auto insert_or_assign_impl(const_node_iterator hint, const auto &key, T &&value) -> std::pair<iterator, bool>
 		{
-			/* See if we can replace any entry. */
+			/* If a candidate was found, replace the entry. Otherwise, emplace a new entry. */
 			const auto h = hash(key);
-			auto *chain_idx = get_chain(h);
-			while (*chain_idx != npos)
-				if (auto &candidate = m_dense[*chain_idx]; candidate.hash == h && cmp(key, candidate.key()))
-				{
-					/* Found a candidate for replacing, replace the value & hash. */
-					if constexpr (requires { candidate.value() = std::forward<T>(value); })
-						candidate.value() = std::forward<T>(value);
-					else
-					{
-						std::destroy_at(candidate.get());
-						std::construct_at(candidate.get(), std::forward<T>(value));
-					}
-					candidate.hash = h;
-					return {to_iter(m_dense.data() + *chain_idx), false};
-				}
-				else
-					chain_idx = &candidate.next;
-
-			/* No candidate for replacement found, create new entry. */
-			return {insert_new(h, chain_idx, std::forward<T>(value)), true};
-		}
-		template<typename... Args>
-		[[nodiscard]] constexpr std::pair<iterator, bool> try_emplace_impl(const auto &key, Args &&...args)
-		{
-			/* See if an entry already exists. */
-			const auto h = hash(key);
-			auto *chain_idx = get_chain(h);
-			while (*chain_idx != npos)
-				if (auto &existing = m_dense[*chain_idx]; existing.hash == h && cmp(key, existing.key()))
-					return {to_iter(m_dense.data() + *chain_idx), false};
-				else
-					chain_idx = &existing.next;
-
-			/* No existing entry found, create new entry. */
-			return {insert_new(h, chain_idx, std::forward<Args>(args)...), true};
-		}
-
-		constexpr void maybe_rehash()
-		{
-			if (load_factor() > max_load_factor)
-				rehash(bucket_count() * 2);
-		}
-		constexpr void rehash_impl(size_type new_cap)
-		{
-			/* Clear & reserve the sparse vector with npos. */
-			m_sparse.clear();
-			m_sparse.resize(new_cap, npos);
-
-			/* Go through each entry & re-insert it. */
-			for (size_type i = 0; i < m_dense.size(); ++i)
+			if (const auto [candidate, chain_idx] = find_node(h, key); *chain_idx == npos)
+				return {emplace_at(hint, chain_idx, h, std::forward<T>(value)), true};
+			else
 			{
-				auto &entry = m_dense[i];
-				auto *chain_idx = get_chain(entry.hash);
-				entry.next = *chain_idx;
-				*chain_idx = i;
+				candidate->replace(std::forward<T>(value), h);
+				return {to_iter(candidate), false};
 			}
 		}
 
@@ -412,6 +517,37 @@ namespace tpp::detail
 				chain_idx = &entry_ptr->next;
 			}
 			return end();
+		}
+
+		constexpr void maybe_rehash()
+		{
+			if (load_factor() > max_load_factor)
+				rehash(bucket_count() * 2);
+		}
+		constexpr void rehash_impl(size_type new_cap)
+		{
+			/* Clear & reserve the sparse vector with npos. */
+			m_sparse.clear();
+			m_sparse.resize(new_cap, npos);
+
+			/* Go through each entry & re-insert it. */
+			for (size_type i = 0; i < m_dense.size(); ++i)
+			{
+				auto &entry = m_dense[i];
+				auto *chain_idx = get_chain(entry.hash);
+				entry.next = *chain_idx;
+				*chain_idx = i;
+			}
+		}
+
+		constexpr void relink_header() noexcept
+		{
+			if constexpr (is_ordered::value)
+			{
+				/* Only relink the header if there are nodes. */
+				if (auto *h = header_link(); !m_dense.empty())
+					h->relink(h->next, h->prev);
+			}
 		}
 
 		sparse_t m_sparse = {initial_capacity, npos};
